18894309ec242f9250bac3c5a098a683b269120a
Manuel <manuel.devito@icloud.com>
2020-11-13 01:23:20 +0100

    Milestone 1

---
+++ main.js
@@ -0,0 +1,187 @@
+
+var app = new Vue ({
+
+    el:'#root',
+
+    data: {
+    contatti: [
+            {
+                nome: 'Michele',
+                avatar: 'http://www.avatars.it/pictures/Mondiali.jpg',
+                visibile: true,
+                messaggi: [
+                    {
+                        data: '12/11/2020 20:30:35',
+                        messaggio: 'Hai portato a spasso il cane',
+                        stato: 'Inviato'
+                    },
+                    {
+                        data: '12/11/2020 20:45:50',
+                        messaggio: 'Ricordati di dargli da mangiare',
+                        stato: 'Inviato'
+                    },
+                    {
+                        data: '12/11/2020 20:55:23',
+                        messaggio: 'Tutto fatto!',
+                        stato: 'Ricevuto'
+                    }
+                ],
+            },
+            {
+                nome: 'Fabio',
+                avatar: 'http://www.avatars.it/pictures/Racing2024.jpg',
+                visibile: true,
+                messaggi: [
+                    {
+                        data: '12/11/2020 20:30:35',
+                        messaggio: 'Hai portato a spasso il cane',
+                        stato: 'Inviato'
+                    },
+                    {
+                        data: '12/11/2020 20:45:50',
+                        messaggio: 'Ricordati di dargli da mangiare',
+                        stato: 'Inviato'
+                    },
+                    {
+                        data: '12/11/2020 20:55:23',
+                        messaggio: 'Tutto fatto!',
+                        stato: 'Ricevuto'
+                    }
+                ],
+            },
+            {
+                nome: 'Samuele',
+                avatar: 'http://www.avatars.it/pictures/Sunset6_800.jpg',
+                visibile: true,
+                messaggi: [
+                    {
+                        data: '12/11/2020 20:30:35',
+                        messaggio: 'Hai portato a spasso il cane',
+                        stato: 'Inviato'
+                    },
+                    {
+                        data: '12/11/2020 20:45:50',
+                        messaggio: 'Ricordati di dargli da mangiare',
+                        stato: 'Inviato'
+                    },
+                    {
+                        data: '12/11/2020 20:55:23',
+                        messaggio: 'Tutto fatto!',
+                        stato: 'Ricevuto'
+                    }
+                ],
+            },
+            {
+                nome: 'Alessandro B.',
+                avatar: 'http://www.avatars.it/pictures/Surfing6_1024_thumb.jpg',
+                visibile: true,
+                messaggi: [
+                    {
+                        data: '12/11/2020 20:30:35',
+                        messaggio: 'Hai portato a spasso il cane',
+                        stato: 'Inviato'
+                    },
+                    {
+                        data: '12/11/2020 20:45:50',
+                        messaggio: 'Ricordati di dargli da mangiare',
+                        stato: 'Inviato'
+                    },
+                    {
+                        data: '12/11/2020 20:55:23',
+                        messaggio: 'Tutto fatto!',
+                        stato: 'Ricevuto'
+                    }
+                ],
+            },
+            {
+                nome: 'Alessandro L.',
+                avatar: 'http://www.avatars.it/pictures/TN_220.jpg',
+                visibile: true,
+                messaggi: [
+                    {
+                        data: '12/11/2020 20:30:35',
+                        messaggio: 'Hai portato a spasso il cane',
+                        stato: 'Inviato'
+                    },
+                    {
+                        data: '12/11/2020 20:45:50',
+                        messaggio: 'Ricordati di dargli da mangiare',
+                        stato: 'Inviato'
+                    },
+                    {
+                        data: '12/11/2020 20:55:23',
+                        messaggio: 'Tutto fatto!',
+                        stato: 'Ricevuto'
+                    }
+                ],
+            },
+            {
+                nome: 'Claudia',
+                avatar: 'http://www.avatars.it/pictures/pampit.jpg',
+                visibile: true,
+                messaggi: [
+                    {
+                        data: '12/11/2020 20:30:35',
+                        messaggio: 'Hai portato a spasso il cane',
+                        stato: 'Inviato'
+                    },
+                    {
+                        data: '12/11/2020 20:45:50',
+                        messaggio: 'Ricordati di dargli da mangiare',
+                        stato: 'Inviato'
+                    },
+                    {
+                        data: '12/11/2020 20:55:23',
+                        messaggio: 'Tutto fatto!',
+                        stato: 'Ricevuto'
+                    }
+                ],
+            },
+            {
+                nome: 'Davide',
+                avatar: 'http://www.avatars.it/pictures/asroma27.jpg',
+                visibile: true,
+                messaggi: [
+                    {
+                        data: '12/11/2020 20:30:35',
+                        messaggio: 'Hai portato a spasso il cane',
+                        stato: 'Inviato'
+                    },
+                    {
+                        data: '12/11/2020 20:45:50',
+                        messaggio: 'Ricordati di dargli da mangiare',
+                        stato: 'Inviato'
+                    },
+                    {
+                        data: '12/11/2020 20:55:23',
+                        messaggio: 'Tutto fatto!',
+                        stato: 'Ricevuto'
+                    }
+                ],
+            },
+            {
+                nome: 'Federico',
+                avatar: 'http://www.avatars.it/pictures/roma-totti_thumb.jpg',
+                visibile: true,
+                messaggi: [
+                    {
+                        data: '12/11/2020 20:30:35',
+                        messaggio: 'Hai portato a spasso il cane',
+                        stato: 'Inviato'
+                    },
+                    {
+                        data: '12/11/2020 20:45:50',
+                        messaggio: 'Ricordati di dargli da mangiare',
+                        stato: 'Inviato'
+                    },
+                    {
+                        data: '12/11/2020 20:55:23',
+                        messaggio: 'Tutto fatto!',
+                        stato: 'Ricevuto'
+                    }
+                ],
+            },
+        ]
+
+    }
+});
